40b529cdabf627ac6082a08b586048783b7f8db6
jamshidjanibekov <jamshidjanibekov0@gmail.com>
2023-07-05 11:11:24 +0500

    Success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,6 +1,25 @@
+import Image from "next/image";
+import {BiErrorAlt} from "react-icons/bi";
+import React from "react";
+import Link from "next/link";
+import {AiFillCheckCircle} from "react-icons/ai";
+
 const Success = () =>{
+
     return(
-        <div>Success</div>
+        <div >
+            <div className='flex justify-start py-2 px-4'>
+                <Image src={'/logo.svg'} alt={'logo'} width={56} height={56} className={"cursor-pointer object-contain"}/>
+            </div>
+            <div className='h-[90vh] flex flex-col justify-center items-center'>
+                <AiFillCheckCircle className = 'w-20 h-20 text-green-500'/>
+                <h1 className='text-2xl md:text-5xl mt-4'>Subscription Completed</h1>
+                <Link href='/' >
+                    <button className='mt-4 bg-green-500 py-4 px-5 rounded'>Dashboard</button>
+                </Link>
+            </div>
+        </div>
     )
+
 }
 export default Success
